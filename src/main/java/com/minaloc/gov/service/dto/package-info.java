c3cd83b4a512b6c702ecdcfa007f8ba925a8274c
shema19974 <musonerwaprince@gmail.com>
2022-07-13 10:43:05 +0200

    Initial version of ecitizenApp generated by generator-jhipster@7.8.1

---
+++ src/main/java/com/minaloc/gov/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.minaloc.gov.service.dto;
---
+++ src/test/java/com/minaloc/gov/IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.minaloc.gov;
+
+import com.minaloc.gov.EcitizenApp;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = EcitizenApp.class)
+public @interface IntegrationTest {
+}
